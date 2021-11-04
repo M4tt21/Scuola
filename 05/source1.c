fbe71b679e6692db39b503752bec177a9f8aee04
M4tt21 <senismattia1@gmail.com>
2021-11-04 16:50:04 +0100

    1

--- 05/source1.c
+++ 05/source1.c
@@ -1,6 +1,21 @@
 #include<stdio.h>
 
+int max(int a, int b){
+    if (a>b)
+    {
+        return a;
+    }
+    return b;
+}
+
 int main(){
-    printf("Hello World 1\n");
+    int n1,n2;
+    printf("\nN.1:");
+    scanf("%d",n1);
+    printf("\nN.2:");
+    scanf("%d",n1);
+
+    printf("%d e` il maggiore!",max(n1,n2));
+
     return 0;
 }
