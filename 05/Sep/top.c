cce47111c1432be51db4b8b4cc8c01c49a51d7ac
M4tt21 <senismattia1@gmail.com>
2021-11-04 17:16:15 +0100

    WOW

---
+++ 05/Sep/top.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include "max.h"
+
+
+int main(){
+    int n1,n2;
+    printf("\nN.1:");
+    scanf("%d",&n1);
+    printf("\nN.2:");
+    scanf("%d",&n1);
+
+    printf("%d e` il maggiore!\n",max(n1,n2));
+
+    return 0;
+}
