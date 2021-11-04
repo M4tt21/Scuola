03de44b2c1744bc2919ae6fba71575dd36c8f4e1
M4tt21 <senismattia1@gmail.com>
2021-11-04 18:12:39 +0100

    w

---
+++ 05/EsMaxMin/Single/maxMin.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int max(int a, int b){
+    if (a>b)
+    {
+        return a;
+    }
+    return b;
+}
+
+int min(int a, int b){
+    if (a<b)
+    {
+        return a;
+    }
+    return b;
+}
+
+int main(){
+    int n1,n2,opt;
+    bool err=false;
+    printf("\nN.1:");
+    scanf("%d",&n1);
+    printf("\nN.2:");
+    scanf("%d",&n1);
+
+    do{
+        err=false;
+        printf("\nTrovare il Maggiore[0] o il Minore[1]:");
+        scanf("%d",&opt);
+        switch (opt)
+        {
+        case 0:
+            printf("%d e` il maggiore!\n",max(n1,n2));
+            break;
+
+        case 1:
+            printf("%d e` il minore!\n",min(n1,n2));
+            break;
+        
+        default:
+            err=true;
+            break;
+        }
+
+    }while(err);
+
+    return 0;
+}
