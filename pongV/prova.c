b440878221aec557625c3340ea8f6f712be53b2d
M4tt21 <senismattia1@gmail.com>
2021-11-11 22:16:54 +0100

    s

--- pongV/prova.c
+++ pongV/prova.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <ncurses.h>
 #include <stdbool.h>
 #include <time.h>
 #include <stdlib.h>
 
 int main() {
+    initscr();
+    noecho();
+    cbreak();
     while (1)
     {
-        printf("%c", getchar());
+        printw("%c", getch());
+        refresh();
     }
     return 0;
 }
