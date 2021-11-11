cafc6d99d3b500a54aa847aa294cd2e2f21b575f
M4tt21 <senismattia1@gmail.com>
2021-11-11 17:46:37 +0100

    s

---
+++ 07/provRimb.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <ncurses.h>
+#include <stdbool.h>
+
+int main() {
+int maxy = 0, maxx = 0;
+int newx = 0, newy = 0;
+int direzionex = 1, direzioney = 1;
+initscr();
+noecho();
+curs_set(false);
+getmaxyx(stdscr, maxy, maxx); /* Ottiene dimensioni schermo */
+int x = maxx/2, y = maxy/2; /* Centro dello schermo */
+while(true) {
+        getmaxyx(stdscr, maxy, maxx); /* Ottiene dimensioni schermo */
+        clear();
+        mvprintw(y, x, "o");
+        refresh();
+        usleep(40000);
+        newx = x + direzionex;
+        newy = y + direzioney;
+        /* Inverte la direzione del movimento toccando il bordo */
+        if(newx >= maxx || newx < 0) direzionex*= -1;
+        else x+= direzionex;
+        if(newy >= maxy || newy < 0) direzioney*= -1;
+        else y+= direzioney;
+        timeout(1); /* Non attende il tasto invio */
+        if(getch() == 113) break; /* Esce alla pressione del tasto 'q' */
+    }
+    endwin();
+}
