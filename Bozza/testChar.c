6db707b31338fc9587c88f9c5f4e7c9b5b907578
M4tt21 <senismattia1@gmail.com>
2021-11-11 17:30:27 +0100

    s

--- Bozza/testChar.c
+++ Bozza/testChar.c
@@ -12,8 +12,8 @@ int main() {
     curs_set(false);
     getmaxyx(stdscr, maxy, maxx);  /* Ottiene dimensioni schermo */
     int x = maxx/2, y = maxy/2;  /* Centro dello schermo */
-    while(true) {
-        printw( "%c : [%d]\n", i, i);
+    while(i<300) {
+        printw("%c : [%d] || ", i, i);
         refresh();
         i++;
         usleep(40000);
