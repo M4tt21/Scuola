04d1f571c8b139aa292e172fe5825ca55c9a27f0
M4tt21 <senismattia1@gmail.com>
2021-11-11 16:39:11 +0100

    s

---
+++ 07/provaBounce.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <ncurses.h>
+#include <stdbool.h>
+int main() {
+    int maxy = 0, maxx = 0;
+    int newx = 0;
+    int direzione = 1;
+    initscr();
+    noecho();
+    curs_set(false);
+    getmaxyx(stdscr, maxy, maxx);  /* Ottiene dimensioni schermo */
+    int x = maxx/2, y = maxy/2;  /* Centro dello schermo */
+    while(true) {
+        clear();
+        mvprintw(y, x, "%219%219");
+        refresh();
+        usleep(40000);
+        newx = x + direzione;
+        /* Inverte la direzione del movimento toccando il bordo */
+        if(newx >= maxx || newx < 0) direzione*= -1;
+        else x+= direzione;
+        timeout(1); /* Non attende il tasto invio */
+        if(getch() == 113) break; /* Esce alla pressione del tasto 'q' */
+    }
+    endwin();
+}
