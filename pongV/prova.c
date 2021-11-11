63cb1b8dff00da9dcf2be35adcb68d9cc7e03c99
M4tt21 <senismattia1@gmail.com>
2021-11-11 23:21:20 +0100

    s

--- pongV/prova.c
+++ pongV/prova.c
@@ -11,9 +11,15 @@ int main() {
     cbreak();
     set_escdelay(0);
     clock_t t=clock();
+    int i;
     while (1)
     {
-        sleep(1);
+        for(i=0;i++;i<100000){
+            int x=i;
+            i+=i*2/3*7000/2;
+            i=x;
+        }
+
         clock_t difference = clock() - t;
         int msec = difference * 1000 / CLOCKS_PER_SEC;
         printw("%i\n", msec);
